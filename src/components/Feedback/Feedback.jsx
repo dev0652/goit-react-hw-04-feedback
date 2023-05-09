7f7acfe55a5a4c3579b38d0b6fe3e46b88e53cad
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-05-09 20:57:51 +0300

    task 1 tmp

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -11,39 +11,65 @@ import {
 
 class Feedback extends Component {
   state = {
-    good: 2,
-    neutral: 3,
-    bad: 5,
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
 
   countTotalFeedback = () => {
     return Object.values(this.state).reduce((prev, value) => prev + value, 0);
   };
 
-  countPositiveFeedbackPercentage = () => {};
+  countPositiveFeedbackPercentage = () => {
+    // good / total * 100%
+    return `${Math.round(
+      (this.state.good / this.countTotalFeedback()) * 100
+    )}%`;
+  };
+
+  updateFeedback = key => {
+    this.setState(prevState => ({
+      [key]: prevState[key] + 1,
+    }));
+  };
 
   render() {
+    const {
+      state,
+      updateFeedback,
+      countTotalFeedback,
+      countPositiveFeedbackPercentage,
+    } = this;
+
     return (
       <Wrapper>
-        <Heading>Please leave your feedback</Heading>
+        <Heading>Rate our service</Heading>
         <ButtonList>
-          {Object.keys(this.state).map(key => (
+          {Object.keys(state).map(key => (
             <ButtonItem key={key}>
-              <Button>{key}</Button>
+              <Button onClick={() => updateFeedback(key)}>{key}</Button>
             </ButtonItem>
           ))}
         </ButtonList>
 
-        <Heading>Statistics</Heading>
-        <StatList>
-          {Object.entries(this.state).map(([key, value]) => (
-            <StatItem key={key}>
-              {key}: {value}
-            </StatItem>
-          ))}
+        {Object.values(state).some(el => el !== 0) && (
+          <>
+            <Heading>Statistics</Heading>
+            <StatList>
+              {Object.entries(state).map(([key, value]) => (
+                <StatItem key={key}>
+                  {key}: {value}
+                </StatItem>
+              ))}
 
-          <StatItem key="total">Total: {this.countTotalFeedback}</StatItem>
-        </StatList>
+              <StatItem key="total">Total: {countTotalFeedback()}</StatItem>
+
+              <StatItem key="percentage">
+                Positive feedback: {countPositiveFeedbackPercentage()}
+              </StatItem>
+            </StatList>
+          </>
+        )}
       </Wrapper>
     );
   }
